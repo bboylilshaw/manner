bc0f2f9098c1808fd106f26d42e066276b72ca64
xiaoyao <bboylilshaw@gmail.com>
2014-10-09 17:35:42 +0800

    added few files

--- src/main/java/org/jshaw/manner/web/controller/HomeController.java
+++ src/main/java/org/jshaw/manner/web/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.jshaw.manner.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -9,6 +10,16 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "home";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup() {
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String doSignup() {
+        return "signup";
     }
 
 }
